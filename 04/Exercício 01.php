be9a388c0c81e083b6f7edbfeef385af23ff33bd
paulinho55-hash <pauloedu.alves55@gmail.com>
2025-09-03 09:20:13 -0300

    Add files via upload

---
+++ 04/Exercício 01.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01</title>
+</head>
+<body>
+    <h1>
+        <p>
+            Cabeçalho tamanho 01
+        </p>
+    </h1>
+
+    <h2>
+        <p>
+            Cabeçalho tamanho 02
+        </p>
+    </h2>
+
+    <h3>
+        <p>
+            Cabeçalho tamanho 03
+        </p>
+    </h3>
+
+    <h4>
+        <p>
+            Cabeçalho tamanho 04
+        </p>
+    </h4>
+
+    <h5>
+        <p>
+            Cabeçalho tamanho 05
+        </p>
+    </h5>
+
+    <h6>
+        <p>
+            Cabeçalho tamanho 06
+        </p>
+    </h6>
+</body>
+</html>
